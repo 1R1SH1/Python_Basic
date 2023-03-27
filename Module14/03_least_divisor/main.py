ce208b4c1fcc358f8845c0e4b52ccc7310ada862
Rishat Murzyev <rmurzyev@mail.ru>
2023-03-27 19:11:41 +0500

    Completed tasks

--- Module14/03_least_divisor/main.py
+++ Module14/03_least_divisor/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def divider():
+  user_number = int(input('Введите число: '))
+  number = 2
+
+  while user_number % number != 0:
+    number += 1
+
+  print('Наименьший делитель, отличный от единицы:', number)
+
+divider()
+
+
