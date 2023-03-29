5c0c3a6bf5e6d3df33d9f0d6489536eca631caf1
Rishat Murzyev <rmurzyev@mail.ru>
2023-03-29 20:13:57 +0500

    Success solutions at second day at this module

--- Module15/04_movie/main.py
+++ Module15/04_movie/main.py
@@ -1,5 +1,16 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+liked_films_list = []
+def liked_films():
+    user_choose_count = int(input('Сколько фильмов хотите добавить? '))
+    for _ in range(user_choose_count):
+        user_film = input('Введите название фильма: ')
+        if user_film in films:
+            liked_films_list.append(user_film)
+        else:
+            print(f'Ошибка: фильма {user_film} у нас нет :(')
 
+    print(f'Ваш список любимых фильмов: {liked_films_list}')
 
+liked_films()
--- Module15/06_running_nums/main.py
+++ Module15/06_running_nums/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def shift():
+    list = []
+    numbers = int(input('Введите кол-во символов в списке: '))
+    for _ in range(numbers):
+        user_number = int(input('Введите число: '))
+        list.append(user_number)
+    shifting = int(input('Сдвиг: '))
+
+    new_list = []
+    for number in range(len(list)):
+        new_list.append(list[number - shifting])
+
+    print(f'Изначальный список: {list}', f'\nСдвинутый список: {new_list}')
+
+shift()
