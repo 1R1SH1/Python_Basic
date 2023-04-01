c1257b9c9d54afa6ec57356c590073eb08b93c38
Rishat Murzyev <rmurzyev@mail.ru>
2023-04-01 22:58:14 +0500

    Finally success solutions at one day at module 18

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+text = input('Введите строку: ').split()
+
+clone = [len(i) for i in text]
+biggest = text[clone.index(max(clone))]
+
+print('Самое длинное слово: {}'.format(biggest))
+print('Длина этого слова: {}'.format(len(biggest)))
+
+
--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+first_s = input('Введите первую стоку: ')
+second_s = input('Введите вторую строку: ')
+k = second_s.index(second_s[0]) + first_s.index(second_s[0])
+
+for position in range(len(second_s)):
+  if second_s[position] != first_s[(position + k) % len(second_s)]:
+    print('Первую строку нельзя получить из второй с помощью циклического сдвига.')
+    break
+else:
+  print('Первая строка получается из второй со сдвигом ', k)
