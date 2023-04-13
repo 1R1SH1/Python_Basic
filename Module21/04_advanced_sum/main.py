59e225556a9f50676614521493b96bfd4c392444
Rishat Murzyev <rmurzyev@mail.ru>
2023-04-13 16:41:57 +0500

    Completed Module 21

--- Module21/04_advanced_sum/main.py
+++ Module21/04_advanced_sum/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def sum(data):
+    result = 0
+    for dat in data:
+        if isinstance(dat, list):
+            result += sum(dat)
+        else:
+            result += dat
+    return result
+
+data_1 = [[1, 2, [3]], [1], 3]
+data_2 = (1, 2, 3, 4, 5)
+
+res = sum(data_1)
+res_2 = sum(data_2)
+print(f'Ответ в консоли: {res}', f'\nОтвет в консоли: {res_2}')
