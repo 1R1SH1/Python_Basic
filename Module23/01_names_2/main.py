545fa229433af4d70b973d8f7cd44a7303179133
Rishat Murzyev <rmurzyev@mail.ru>
2023-04-16 13:19:17 +0500

    Completed Module 23

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def name_checker(data):
+  with open('people.txt', encoding='UTF-8') as people_l:
+    try:
+        people_l = people_l.readlines()
+        size = sum([len(i.strip()) for i in people_l])
+        for x, i in enumerate(people_l, 1):
+            if len(i.strip()) < 3:
+                raise BaseException(f'Ошибка: менее трёх символов в строке {x}')
+    except BaseException as exp:
+        print(exp)
+    finally:
+        print('Общее количество символов:', size)
+
+name_checker('people.txt')
--- Module23/02_lucky_number/main.py
+++ Module23/02_lucky_number/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+import random
+
+
+def luky_num():
+    nums, file_l = 0, open('out_file.txt', 'w')
+
+    with file_l as file:
+        while nums <= 777:
+            user_input = int(input('Введите число: '))
+            nums += user_input
+            if random.choices((0, 1), (1 - 1 / 13, 1 / 13))[0]:
+                raise BaseException('Ой, вам повезло, надо же вы поймали ошибку!')
+
+            print(user_input, file=file)
+
+        print('Вы успешно выполнили условие для выхода из порочного цикла!')
+
+    print_file = open("out_file.txt", "r")
+    data = print_file.read()
+    print('Содержимое файла out_file.txt: ')
+    print(data)
+    print_file.close()
+
+
+luky_num()
