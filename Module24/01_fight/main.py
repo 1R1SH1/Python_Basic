4e339922334acd8a8e5b6860eb4efe3273536162
Rishat Murzyev <rmurzyev@mail.ru>
2023-04-17 17:20:40 +0500

    Completed Module 24

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+import random
+
+class Warrior:
+  def __init__(self):
+    self.hp_warrior = 0
+
+warrior_1 = Warrior()
+warrior_2 = Warrior()
+warrior_1.hp_warrior = 100
+warrior_2.hp_warrior = 100
+
+while True:
+  who_beats = random.randint(0, 10)
+  if warrior_1.hp_warrior == 0:
+    print('Победил Ришат')
+    break
+  elif warrior_2.hp_warrior == 0:
+    print('Победил Куратор')
+    break
+  elif who_beats < 6:
+    warrior_2.hp_warrior -= 20
+    print('Куратор дубасит Ришата')
+    print(f'У Ришата осталось {warrior_2.hp_warrior} здоровья\n')
+  elif who_beats > 5:
+    warrior_1.hp_warrior -= 20
+    print('Ришат лупит Куратора')
+    print(f'У Куратора осталось {warrior_1.hp_warrior} здоровья\n')
--- Module24/05_cohabitation/main.py
+++ Module24/05_cohabitation/main.py
@@ -1,1 +1,63 @@
-# TODO здесь писать код
+import random
+
+
+class House:
+    food = 50
+    money = 0
+
+
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+        self.satiety = 50
+
+    def eat(self):
+        self.satiety += 1
+        House.food -= 1
+        return f'ест, сытость {self.satiety} еда {House.food}'
+
+    def work(self):
+        self.satiety -= 1
+        House.money += 1
+        return f'работает, сытость {self.satiety} деньги {House.money}'
+
+    def play(self):
+        self.satiety -= 1
+        return f'играет, сытость {self.satiety}'
+
+    def repast(self):
+        House.food += 1
+        House.money -= 1
+        return f'идет в магазин, еда {House.food} деньги {House.money}'
+
+
+def play(person):
+    number_cubes = random.randint(1, 6)
+    if person.satiety < 0:
+        print(f'К сожалению, {person.name} помер с голоду ')
+        return 1
+    if person.satiety < 20 and House.food >= 10:
+        text = person.eat()
+    elif House.food < 10:
+        text = person.repast()
+    elif House.money < 50:
+        text = person.work()
+    elif number_cubes == 1:
+        text = person.work()
+    elif number_cubes == 2:
+        text = person.eat()
+    else:
+        text = person.play()
+    print(person.name, text)
+    return 0
+
+
+person_1 = Person('Вася')
+person_2 = Person('Федя')
+
+for i in range(365):
+    if play(person_1) or play(person_2):
+        print('все плохо')
+if i == 364:
+    print('выжили')
