5b548568abd49fefde1054b3ec71f63a129b183a
Rishat Murzyev <rmurzyev@mail.ru>
2023-04-18 16:19:54 +0500

    Completed Module 25

--- Module25/01_taxes/main.py
+++ Module25/01_taxes/main.py
@@ -1,1 +1,47 @@
-# TODO здесь писать код
+class Property():
+    tax = 0
+
+    def __init__(self, __worth):
+        self.worth = __worth
+
+    def __str__(self):
+        return '{}:\n-Стоимость равняется: {}.\n-Налог на соответствующее имущество {}'
+
+    def get_tax(self):
+        return round(self.worth / self.tax, 2)
+        pass
+
+
+class Apartment(Property):
+    tax = 1000
+
+    def __str__(self):
+        return super().__str__().format('Апартаменты', self.worth, self.get_tax())
+
+
+class Car(Property):
+    tax = 200
+
+    def __str__(self):
+        return super().__str__().format('Машина', self.worth, self.get_tax())
+
+
+class CountryHouse(Property):
+    tax = 500
+
+    def __str__(self):
+        return super().__str__().format('Дача', self.worth, self.get_tax())
+
+
+apartment = Apartment(int(input('Введите стоимость квартиры: ')))
+car = Car(int(input('Введите стоимость машины: ')))
+country_house = CountryHouse(int(input('Введите стоимость дачи: ')))
+money = int(input('Cколько у вас есть денег: '))
+print(apartment)
+print(car)
+print(country_house)
+sum_tax = round(sum(i_property.get_tax() for i_property in (apartment, car, country_house)))
+if sum_tax > money:
+    print('Вам не хватает', abs(sum_tax - money))
+else:
+    print('Собственных средств хватит на покрытие налогов')
--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,1 +1,36 @@
-# TODO здесь писать код
+import random
+
+class __KillError(Exception):
+    pass
+
+class __DrunkError(Exception):
+    pass
+
+class __CarCrashError(Exception):
+    pass
+
+class __GluttonyError(Exception):
+    pass
+
+class __DepressionError(Exception):
+    pass
+
+def life_simulater():
+    karma = 0
+    with open('karmaLog.txt', 'w') as log:
+        while karma < 500:
+            try:
+                not_lucky = random.choices((0, 1), (1 - 1 / 10, 1 / 10))[0]
+
+                if not_lucky:
+                    raise Exception
+                else:
+                    karma += random.randint(1, 7)
+                    print(f'Карма = {karma}')
+            except Exception:
+                log.write(str(random.choice(
+                    [__DepressionError, __GluttonyError, __CarCrashError, __KillError, __DrunkError])) + '\n')
+
+        print('Вы достигли просветления!')
+
+life_simulater()
