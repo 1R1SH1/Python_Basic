3e296f56948eed06b315204eded9c1b028da4ab8
Rishat Murzyev <rmurzyev@mail.ru>
2023-04-19 16:59:12 +0500

    Completed Module 26

--- Module26/02_files_path/main.py
+++ Module26/02_files_path/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import os
+
+folder = os.path.abspath(os.path.join('..'))
+
+gen_files_path = [links + '\\' + file
+              for links, dirs, files in list(os.walk(folder))
+              for file in files
+              ]
+
+for file in gen_files_path:
+  print(file)
