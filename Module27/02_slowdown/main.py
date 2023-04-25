bf9214d15b222bc5f8eace13707908110c17a42f
Rishat Murzyev <rmurzyev@mail.ru>
2023-04-25 19:11:28 +0500

    Corrected Module 27

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -2,14 +2,19 @@ import time
 from typing import Callable, Any
 
 def waiting(func: Callable) -> Callable:
-  def timing() -> Any:
+  def wrapped_func(*args, **kwargs) -> Any:
     print('Идёт загрузка...ждите!')
     time.sleep(3)
-    func()
-  return timing
+    result = func(*args, **kwargs)
+    return result
+  return wrapped_func
 
+@waiting
+def sum(a, b):
+  return a + b
 @waiting
 def test():
   print('Загрузка завершена!')
 
+print(sum(1, 2))
 test()
